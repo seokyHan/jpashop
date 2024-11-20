0ade20cc4dbede9278f54864b754438560282f2a
seokyHan <sseok1979@naver.com>
2024-11-20 11:24:57 +0900

    add : 회원 관련 서비스 추가

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //회원 가입
+    @Transactional
+    public Long join (Member member) {
+        validateDuplicatedMember(member); // 동시성 고려(멀티 쓰레드, name에 유니크 제약 조건)
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        List<Member> members = memberRepository.findByName(member.getName());
+        if(!members.isEmpty()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Member member) {
+        return memberRepository.findOne(member.getId());
+    }
+
+}
